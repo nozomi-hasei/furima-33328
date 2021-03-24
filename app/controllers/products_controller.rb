7a31397744195afff1e5dd60eacd47b25054d237
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2021-03-24 21:40:45 +0900

    再レビュー修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   
   def index
-    @products = Product.all.order("created_at DESC")
+    @products = Product.order("created_at DESC")
   end
 
   def new
