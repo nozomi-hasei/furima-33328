5873cd46a4ed83c4b013eea7d1e2fc4318f37e80
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2021-04-26 17:13:48 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock "~> 3.16.0"
 
 set :application, "furima-33328"
-set :repo_url, "git@example.com:nozomi-hasei/furima-33328.git"
+set :repo_url, "git@github.com:nozomi-hasei/furima-33328.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
