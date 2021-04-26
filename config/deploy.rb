3965f14a3ceb635c6f818b73d0cfd2d7c620c9fb
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2021-04-26 10:36:59 +0900

    Capistranoの導入準備

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+lock "~> 3.1"
+
+set :application, "furima-33328"
+set :repo_url, "git@example.com:nozomi-hasei/furima-33328.git"
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/nozomi.pem'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
+
